ad8ba9cace3f0346fb45cfec770a9c813466e259
AleksandrMikhalevich <aleksandr.mikhalevich89@gmail.com>
2023-09-09 16:50:08 +0300

    Refactored

--- src/main/java/by/iit/App.java
+++ src/main/java/by/iit/App.java
@@ -1,35 +1,30 @@
 package by.iit;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class App {
+
     public static void main(String[] args) {
-        try (Statement statement = App.getConnection().createStatement()) {
-            try (ResultSet resultSet = statement.executeQuery("select * from users")) {
-                while (resultSet.next()) {
-                    System.out.println(resultSet.getInt(1) + " "
-                            + resultSet.getString(2) + " "
-                            + resultSet.getString(3) + " "
-                            + resultSet.getString(4) + " "
-                            + resultSet.getInt(5));
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        try (Statement statement = JdbcUtils.getConnection().createStatement()) {
+            createQuery(statement);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static Connection getConnection() throws SQLException {
-        Connection conn;
-        conn = DriverManager.getConnection(JdbcUtils.PROPERTIES.getProperty("url"),
-                JdbcUtils.PROPERTIES.getProperty("user"),
-                JdbcUtils.PROPERTIES.getProperty("password"));
-        return conn;
+    private static void createQuery(Statement statement) {
+        try (ResultSet resultSet = statement.executeQuery("select * from users")) {
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " "
+                        + resultSet.getString(2) + " "
+                        + resultSet.getString(3) + " "
+                        + resultSet.getString(4) + " "
+                        + resultSet.getInt(5));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/by/iit/JdbcUtils.java
+++ src/main/java/by/iit/JdbcUtils.java
@@ -2,6 +2,9 @@ package by.iit;
 
 import uk.org.webcompere.lightweightconfig.ConfigLoader;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class JdbcUtils {
@@ -9,6 +12,12 @@ public class JdbcUtils {
     private JdbcUtils() {
     }
 
-    protected static final Properties PROPERTIES = ConfigLoader.loadPropertiesFromResource(
+    private static final Properties PROPERTIES = ConfigLoader.loadPropertiesFromResource(
             "school_db.properties");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(PROPERTIES.getProperty("url"),
+                PROPERTIES.getProperty("user"),
+                PROPERTIES.getProperty("password"));
+    }
 }
